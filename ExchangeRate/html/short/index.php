002a736e22fbfbcdf139d08b9717d9a136bab875
U-LucianSirbu-PC\Lucian Sirbu <Lucian Sirbu@LucianSirbu-PC.(none)>
2014-02-11 09:51:25 +0200

    The script now displays basic info, no stylng done

--- ExchangeRate/html/short/index.php
+++ ExchangeRate/html/short/index.php
@@ -1,19 +1,59 @@
 <?php
-$fullData = json_decode(str_replace(Array("ExchangeRateData = ", "}}}}};"), Array("", "}}}}}"), file_get_contents("../ExchangeRate-data.js")));
+
+ini_set('display_errors', '1');
+error_reporting(-1);
+
+date_default_timezone_set('UTC');
+
+$fullXchData = json_decode(str_replace(Array("ExchangeRateData = ", "}}}}};"), Array("", "}}}}}"), file_get_contents("../ExchangeRate-data.js")));
 
 $stats = Array();
+$stats['perDay'] = Array();
+$stats['recordedItems'] = 0;
 
 $lastData = null;
-foreach($fullData as $k=>$data) {
-	$dt = DateTime::createFromFormat('Y-m-dH:i:sT', str_replace("T", "", $data->date));
+foreach($fullXchData as $k=>$xch) {
+	$dt = DateTime::createFromFormat('Y-m-dH:i:sT', str_replace("T", "", $xch->date));
+	
+	if(!isset($stats['perDay'][$dt->format('Y-m-d')])) {
+		$stats['perDay'][$dt->format('Y-m-d')] = Array();
+		$stats['perDay'][$dt->format('Y-m-d')]['recordedItems'] = 0;
+	}
 	$stats['perDay'][$dt->format('Y-m-d')]['recordedItems']++;
 	$stats['recordedItems']++;
 	
-	$lastData = $data;
+	$lastXchData = $xch;
 }
 
-$lastData->date = DateTime::createFromFormat('Y-m-dH:i:sT', str_replace("T", "", $lastData->date));
-var_dump($lastData);
+$lastXchData->date = DateTime::createFromFormat('Y-m-dH:i:sT', str_replace("T", "", $lastXchData->date));
+
+$sortedSellXchData = Array();
+foreach ($lastXchData->values as $bank=>$bxch) {
+	foreach($bxch as $currency=>$xch) {
+		if ($bank!='BNR') {
+			$sortedSellXchData[$currency][$bank] = $xch->sell;
+		}
+	}
+}
+
+
+
+foreach($sortedSellXchData as $currency=>$x) {
+	$v = $sortedSellXchData[$currency];
+	asort($v);
+	$sortedSellXchData[$currency] = $v;
+}
+$x = $sortedSellXchData;
+$sortedSellXchData = Array();
+foreach(Array('EUR', 'USD', 'GBP', 'CHF', ) as $k) {
+	$sortedSellXchData[$k] = $x[$k];
+	unset($x[$k]);
+}
+foreach($x as $k=>$v) {
+	$sortedSellXchData[$k] = $x[$k];
+	unset($x[$k]);
+}
+
 
 ?>
 <!doctype html>
@@ -43,15 +83,26 @@ var_dump($lastData);
 		</div>
 	</div>
 	
-	<div class='lastData'>
+	<div class='lastXchData'>
 		<div>
 			<span class='label'>date</span>
-			<span class='value'><?=$lastData->date->format("d M, H:i")?></span>
+			<span class='value'><?=$lastXchData->date->format("d M, H:i");?></span>
 		</div>
 		
 		<div>
-			<span class='label'>EUR</span>
-			<span class='value'><?=number_format($lastData->values->BNR->EUR->sell, 4)?></span>
+			<?php foreach($sortedSellXchData as $currency=>$xchData) { ?>
+				<div class='data <?=$currency?>'>
+					<span class='label'><?=$currency?></span>
+					<span class='value'><?=number_format($lastXchData->values->BNR->{$currency}->sell, 4)?></span>
+			
+					<div class='banks'>
+						<?php foreach($xchData as $bank=>$value) { ?>
+							<span class='label'><?=$bank?></span>
+							<span class='value'><?=number_format($value, 4)?></span>
+						<?php } ?>
+					</div>
+				</div>
+			<?php } ?>
 		</div>
 	</div>
 </body>
